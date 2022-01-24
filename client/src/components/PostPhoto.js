a581d94553cde7b79a0a0d4df8d5ab128bfa9afe
Max Mezalon <jmezalon@gmail.com>
2022-01-24 12:29:13 -0500

    add hashtags is working

--- client/src/components/PostPhoto.js
+++ client/src/components/PostPhoto.js
@@ -1,11 +1,56 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import FooterSecondary from "./FooterSecondary";
 import "../styles/profile.css";
+import {
+  fetchHashtags,
+  resetPopularHash,
+  // fetchSingleTag,
+} from "../features/hashtags/hashtagsSlice";
 
 function PostPhoto() {
-  const user = useSelector((state) => state.users.user);
-  // const hashtag
-  console.log(user);
+  const dispatch = useDispatch();
+  // const user = useSelector((state) => state.users.user);
+  const hashtags = useSelector((state) => state.hashtags.hashtags);
+  const [tagInput, setTagInput] = useState("");
+  const [tags, setTags] = useState([]);
+  const [tagLeft, setTagLeft] = useState(3);
+  const [img_url, setImg_url] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchHashtags());
+    return () => {
+      dispatch(resetPopularHash());
+    };
+  }, [dispatch]);
+
+  let availableHash = hashtags.filter((tag) =>
+    tag.tag.toLowerCase().includes(tagInput.toLowerCase())
+  );
+
+  function handleDeleteTag(id) {
+    const updateTag = tags.filter((t) => t.id !== id);
+    setTags(updateTag);
+  }
+
+  function handlePhotoSubmit(e) {
+    e.preventDefault();
+    console.log("ran");
+    setImg_url("");
+    setTags([]);
+  }
+
+  function handleTagSubmit() {
+    if (tagInput.toLowerCase() === availableHash[0].tag) {
+      setTags([...tags, availableHash[0]]);
+      setTagInput("");
+      setTagLeft(() => tagLeft - 1);
+      console.log(tagLeft);
+    }
+  }
+
+  const displayNone = tagLeft === 0 ? "none" : "block";
+
   return (
     <>
       <div className="post-photo-container">
@@ -14,10 +59,10 @@ function PostPhoto() {
           <hr />
           <section className="photo-section-and-form">
             <img
-              src={`${process.env.PUBLIC_URL}/foto-icon.png`}
+              src={img_url || `${process.env.PUBLIC_URL}/foto-icon.png`}
               alt="camara-placeholder"
             />
-            <form action="post-photo-form">
+            <form action="post-photo-form" onSubmit={handlePhotoSubmit}>
               <label htmlFor="upload">
                 <input
                   id="upload-button"
@@ -30,16 +75,64 @@ function PostPhoto() {
                 <input
                   type="text"
                   name="img_url"
-                  id=""
+                  onChange={(e) => setImg_url(e.target.value)}
+                  value={img_url}
                   placeholder="Add Photo Link"
                 />
               </label>
+              <div className="selected-tags-container">
+                {tags.length > 0 &&
+                  tags.map((t) => (
+                    <div className="selected-tags" key={t.id}>
+                      <p>{t.tag}</p>
+                      <p id="delete-tag" onClick={() => handleDeleteTag(t.id)}>
+                        x
+                      </p>
+                    </div>
+                  ))}
+              </div>
               <label htmlFor="hashtags">
-                <input type="text" name="hashtags" placeholder="Add Hashtags" />
+                <input
+                  type="text"
+                  name="hashtags"
+                  value={tagInput}
+                  onChange={(e) => setTagInput(e.target.value)}
+                  onKeyPress={(e) => {
+                    availableHash[0] && e.key === "Enter" && handleTagSubmit();
+                  }}
+                  placeholder="Add Hashtags"
+                />
               </label>
-              <p style={{ margin: "-8px 0 35px 108px", fontSize: "smaller" }}>
-                ** 3 minimum required **
-              </p>
+              <div className="available-tag">
+                {tagInput.length >= 2 &&
+                  availableHash.map((t) => (
+                    <li
+                      onClick={() => {
+                        setTags([...tags, t]);
+                        setTagInput("");
+                        setTagLeft(() => tagLeft - 1);
+                      }}
+                      key={t.id}
+                    >
+                      {t.tag}
+                    </li>
+                  ))}
+              </div>
+              {tags.length === 0 && !tagInput ? (
+                <p style={{ margin: "-8px 0 35px 108px", fontSize: "smaller" }}>
+                  ** 3 minimum required **
+                </p>
+              ) : (
+                <p
+                  style={{
+                    margin: "-8px 0 35px 108px",
+                    fontSize: "smaller",
+                    display: `${displayNone}`,
+                  }}
+                >
+                  ** {tagLeft} remaining **
+                </p>
+              )}
               <label htmlFor="confirm">
                 <input id="confirm-button" type="button" value="confirm" />
               </label>
