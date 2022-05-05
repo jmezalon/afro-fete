7f776c7b47da77f4af599b147059f354ec5772c0
Max Mezalon <jmezalon@gmail.com>
2022-05-05 13:11:11 -0400

    converting to sass styling, fixed up navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { NavLink, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import "../styles/nav.css";
+import "../styles/nav.scss";
 import { userLogout } from "../features/users/usersSlice";
 import { showEvents } from "../features/events/eventsSlice";
 import { resetSingleTag } from "../features/hashtags/hashtagsSlice";
@@ -32,47 +32,61 @@ function Navbar() {
   }
 
   return (
-    <nav className="nav-container">
+    <nav className="navbar">
       <div
         onClick={() => {
           dispatch(showEvents(true));
           dispatch(resetSingleTag());
           dispatch(resetFavorite());
         }}
-        className="left-side-nav"
+        className="navbar__left-side"
       >
         <NavLink exact to="/">
           <img
-            className="logo"
+            className="navbar__logo"
             src={process.env.PUBLIC_URL + "/afrofete_logo.png"}
             alt="logo"
           />
         </NavLink>
       </div>
-      <div className="right-side-nav">
-        <NavLink exact to="/about-us">
+      <div className="navbar__right-side">
+        <NavLink
+          className="navbar__right-side__non-button-links"
+          exact
+          to="/about-us"
+        >
           <p>About Us</p>
         </NavLink>
-        <NavLink to="/how-it-works">
+        <NavLink
+          className="navbar__right-side__non-button-links"
+          to="/how-it-works"
+        >
           <p>How It Works</p>
         </NavLink>
         {!user && (
-          <NavLink id="login-active" to="/auth/login">
-            <button>Login</button>
-          </NavLink>
-        )}
-        {!user && (
-          <NavLink id="signup-active" to="/auth/signup">
-            <button>Sign Up</button>
-          </NavLink>
+          <div className="navbar__right-side__auth-buttons">
+            <NavLink to="/auth/login">
+              <button>Login</button>
+            </NavLink>
+
+            <NavLink to="/auth/signup">
+              <button>Sign Up</button>
+            </NavLink>
+          </div>
         )}
         {user && (
-          <NavLink to="/post-a-photo">
+          <NavLink
+            to="/post-a-photo"
+            className="navbar__right-side__non-button-links"
+          >
             <p>Post A Photo</p>
           </NavLink>
         )}
         {user && (
-          <NavLink to="/photo-gallery">
+          <NavLink
+            to="/photo-gallery"
+            className="navbar__right-side__non-button-links"
+          >
             <p>Photo Gallery</p>
           </NavLink>
         )}
